5e396152e9b46fb9b74524a16df0ab280785666d
Shahla Jahan <shahlajahan1982@gmail.com>
2026-07-12 11:34:49 +0300

    Initial project

---
+++ bin/devaudit.dart
@@ -0,0 +1,5 @@
+import 'package:devaudit/devaudit.dart' as devaudit;
+
+void main(List<String> arguments) {
+  print('Hello world: ${devaudit.calculate()}!');
+}
---
+++ test/devaudit_test.dart
@@ -0,0 +1,8 @@
+import 'package:devaudit/devaudit.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
